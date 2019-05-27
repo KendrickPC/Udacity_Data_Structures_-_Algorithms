86dd489e5c48082ad1b5e3305f2435f086001170
kendrickpc <kennethpchang@gmail.com>
2019-05-27 14:15:32 -0700

    linked_list_practice.py complete.

--- Lesson2/hamming_distance.py
+++ Lesson2/hamming_distance.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
+# PEP8 Verified
 
-# Code
 
 def hamming_distance(str1, str2):
     if len(str1) == len(str2):
@@ -9,16 +9,15 @@ def hamming_distance(str1, str2):
             if str1[char] != str2[char]:
                 count += 1
         return count
-    # return none
-    
-    # pass
+
 
 # Test Cases
-
-print ("Pass" if (10 == hamming_distance('ACTTGACCGGG','GATCCGGTACA')) else "Fail")
-print ("Pass" if  (1 == hamming_distance('shove','stove')) else "Fail")
-print ("Pass" if  (None == hamming_distance('Slot machines', 'Cash lost in me')) else "Fail")
-print ("Pass" if  (9 == hamming_distance('A gentleman','Elegant men')) else "Fail")
-print ("Pass" if  (2 == hamming_distance('0101010100011101','0101010100010001')) else "Fail")
-
-
+print ("Pass" if (10 == hamming_distance('ACTTGACCGGG',
+                                         'GATCCGGTACA')) else "Fail")
+print ("Pass" if (1 == hamming_distance('shove', 'stove')) else "Fail")
+print ("Pass" if (None == hamming_distance('Slot machines',
+                                           'Cash lost in me')) else "Fail")
+print ("Pass" if (9 == hamming_distance('A gentleman',
+                                        'Elegant men')) else "Fail")
+print ("Pass" if (2 == hamming_distance('0101010100011101',
+                                        '0101010100010001')) else "Fail")
